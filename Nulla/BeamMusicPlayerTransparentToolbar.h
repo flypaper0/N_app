26e3779d424406e27635c3e380de904a584f9b96
flypaper0 <flypaper.g@mail.ru>
2014-08-01 00:47:48 +0400

    fix file's trash

---
+++ Nulla/BeamMusicPlayerTransparentToolbar.h
@@ -0,0 +1,15 @@
+//
+//  BeamMusicPlayerTransparentToolbar.h
+//  BeamMusicPlayerExample
+//
+//  Created by Heiko Behrens on 21.05.13.
+//  Copyright (c) 2013 n/a. All rights reserved.
+
+
+// Спер Тулбар, не помню зачем. Симпатичный
+
+#import <UIKit/UIKit.h>
+
+@interface BeamMusicPlayerTransparentToolbar : UIToolbar
+
+@end
